08ee0da0a06aefce308403445e6da7e3b03d7cbb
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-07 04:13:09 +0000

    Finished counting to any number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,15 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // make scanner for input
+    Scanner input = new Scanner(System.in);
+    // ask for integer and make its variable
+    System.out.println("Please enter a positive integer");
+    int num = input.nextInt();
+    // for loop and answers
+    for(int count=1; count<num; count++){
+      System.out.print(count + "," + " ");
+    }
+    System.out.print(num);
   }
 }
